ff3ace66eb05c74e2fe6969077032f0b761c89ea
Dick Wolff <dckwlff@gmail.com>
2024-02-02 11:05:25 +0100

    Rename some files, add error handling to manual.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createAndRunConverter } from "./converter";
-
-require("dotenv").config();
-
-// Define import file path.
-const inputFile = process.env.INPUT_FILE;
-
-// Determine convertor type and run conversion.
-createAndRunConverter(process.argv[2].toLocaleLowerCase(), inputFile, ".", () => { }, () => { });
---
+++ src/manual.ts
@@ -0,0 +1,16 @@
+import { createAndRunConverter } from "./converter";
+
+// Check if converter was specified.
+if (process.argv.length != 3) {
+    console.log("[e] Invalid run command: converter not specified!");;
+}
+else {
+
+    require("dotenv").config();
+
+    // Define import file path.
+    const inputFile = process.env.INPUT_FILE;
+
+    // Determine convertor type and run conversion.
+    createAndRunConverter(process.argv[2].toLocaleLowerCase(), inputFile, ".", () => { }, () => { });
+}
