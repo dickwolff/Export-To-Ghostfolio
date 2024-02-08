757aad4b1cfefef557d7163896cdc85e8fa3077b
Dick Wolff <5620002+dickwolff@users.noreply.github.com>
2024-02-08 18:21:03 +0100

    Add tests for eToro converter

--- src/converters/etoroConverter.test.ts
+++ src/converters/etoroConverter.test.ts
@@ -10,4 +10,58 @@ describe("etoroConverter", () => {
       // Asssert
       expect(sut).toBeTruthy();
     });
+
+    it("should process sample CSV file", (done) => {
+
+    // Act
+    const sut = new EtoroConverter();
+    const inputFile = "sample-etoro-export.csv";
+
+    // Act      
+    sut.readAndProcessFile(inputFile, (actualExport: GhostfolioExport) =>  {
+
+      // Assert
+      expect(actualExport).toBeTruthy();
+      
+      // Finish the test
+      done();
+    }, () => { fail("Should not have an error!"); });      
+  });
+
+  describe("should throw an error if", () => {
+    it("the input file does not exist", (done) => {
+
+      // Act
+      const sut = new EtoroConverter();
+
+      let tempFileName = "tmp/testinput/etoro-filedoesnotexist.csv";
+      
+      // Act
+      sut.readAndProcessFile(tempFileName, () =>  { fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        done();
+      });      
+    });
+
+    it("the input file is empty", (done) => {
+
+      // Act
+      const sut = new EtoroConverter();
+
+      // Create temp file.
+      let tempFileContent = "";
+      tempFileContent += "Date,Type,Details,Amount,Units,Realized Equity Change,Realized Equity,Balance,Position ID,Asset type,NWA\n";      
+      
+      // Act
+      sut.processFileContents(tempFileContent, () =>  { fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toContain("An error ocurred while parsing")
+        done();
+      });      
+    });
+  });
 });
