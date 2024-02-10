6d2ecf82462f010a951b7abd3091e4675910724e
Dick Wolff <dckwlff@gmail.com>
2024-02-10 10:08:31 +0100

    Add tests for DEGIRO v2

--- src/converters/degiroConverterV2.test.ts
+++ src/converters/degiroConverterV2.test.ts
@@ -1,14 +1,120 @@
-import { YahooFinanceService } from "../yahooFinanceService";
 import { DeGiroConverterV2 } from "./degiroConverterV2";
+import { YahooFinanceService } from "../yahooFinanceService";
+import { GhostfolioExport } from "../models/ghostfolioExport";
 
-describe("degiroConverterV2", () => {
+fdescribe("degiroConverterV2", () => {
 
-    it("should construct", () => {
+  it("should construct", () => {
 
-      // Act
-      const sut = new DeGiroConverterV2(new YahooFinanceService());
+    // Act
+    const sut = new DeGiroConverterV2(new YahooFinanceService());
+
+    // Assert
+    expect(sut).toBeTruthy();
+  });
+
+  it("should process sample CSV file", (done) => {
+
+    // Arange
+    const sut = new DeGiroConverterV2(new YahooFinanceService());
+    const inputFile = "sample-degiro-export.csv";
+
+    // Act      
+    sut.readAndProcessFile(inputFile, (actualExport: GhostfolioExport) => {
 
       // Assert
-      expect(sut).toBeTruthy();
+      expect(actualExport).toBeTruthy();
+      expect(actualExport.activities.length).toBeGreaterThan(0);
+      expect(actualExport.activities.length).toBe(14);
+
+      done();
+    }, () => { done.fail("Should not have an error!"); });
+  });
+
+  describe("should throw an error if", () => {
+    it("the input file does not exist", (done) => {
+
+      // Arrange
+      const sut = new DeGiroConverterV2(new YahooFinanceService());
+
+      let tempFileName = "tmp/testinput/degiro-filedoesnotexist.csv";
+
+      // Act
+      sut.readAndProcessFile(tempFileName, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+
+        done();
+      });
     });
+
+    it("the input file is empty", (done) => {
+
+      // Arrange
+      const sut = new DeGiroConverterV2(new YahooFinanceService());
+
+      let tempFileContent = "";
+      tempFileContent += "Datum,Tijd,Valutadatum,Product,ISIN,Omschrijving,FX,Mutatie,,Saldo,,Order Id\n";
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toContain("An error ocurred while parsing");
+
+        done();
+      });
+    });
+
+    it("Yahoo Finance throws an error", (done) => {
+
+      // Arrange
+      let tempFileContent = "";
+      tempFileContent += "Datum,Tijd,Valutadatum,Product,ISIN,Omschrijving,FX,Mutatie,,Saldo,,Order Id\n";
+      tempFileContent += `15-12-2022,16:55,15-12-2022,VICI PROPERTIES INC. C,US9256521090,DEGIRO Transactiekosten en/of kosten van derden,,EUR,"-1,00",EUR,"31,98",5925d76b-eb36-46e3-b017-a61a6d03c3e7\n`;
+      tempFileContent += `15-12-2022,16:55,15-12-2022,VICI PROPERTIES INC. C,US9256521090,"Koop 1 @ 33,9 USD",,USD,"-33,90",USD,"-33,90",5925d76b-eb36-46e3-b017-a61a6d03c3e7`;
+
+      // Mock Yahoo Finance service to throw error.
+      const yahooFinanceService = new YahooFinanceService();
+      jest.spyOn(yahooFinanceService, "getSecurity").mockImplementation(() => { throw new Error("Unit test error"); });
+      const sut = new DeGiroConverterV2(yahooFinanceService);
+
+      // Act      
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toContain("Unit test error");
+
+        done();
+      });
+    });
+  });
+
+  it("should log when Yahoo Finance returns no symbol", (done) => {
+
+    // Arrange
+    let tempFileContent = "";
+    tempFileContent += "Datum,Tijd,Valutadatum,Product,ISIN,Omschrijving,FX,Mutatie,,Saldo,,Order Id\n";
+    tempFileContent += `15-12-2022,16:55,15-12-2022,VICI PROPERTIES INC. C,US9256521090,DEGIRO Transactiekosten en/of kosten van derden,,EUR,"-1,00",EUR,"31,98",5925d76b-eb36-46e3-b017-a61a6d03c3e7\n`;
+    tempFileContent += `15-12-2022,16:55,15-12-2022,VICI PROPERTIES INC. C,US9256521090,"Koop 1 @ 33,9 USD",,USD,"-33,90",USD,"-33,90",5925d76b-eb36-46e3-b017-a61a6d03c3e7`;
+
+    // Mock Yahoo Finance service to return null.
+    const yahooFinanceService = new YahooFinanceService();
+    jest.spyOn(yahooFinanceService, "getSecurity").mockImplementation(() => { return null });
+    const sut = new DeGiroConverterV2(yahooFinanceService);
+
+    // Bit hacky, but it works.
+    const consoleSpy = jest.spyOn((sut as any).progress, "log");
+
+    // Act      
+    sut.processFileContents(tempFileContent, () => {
+
+      expect(consoleSpy).toHaveBeenCalledWith("[i] No result found for US9256521090 with currency EUR! Please add this manually..\n");
+
+      done();
+    }, () => done.fail("Should not have an error!"));
+  });
 });
