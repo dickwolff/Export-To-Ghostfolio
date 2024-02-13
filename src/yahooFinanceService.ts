81dbdf29d2f5cbf37625b1b856dbf48bc2bbafba
Dick Wolff <5620002+dickwolff@users.noreply.github.com>
2024-02-13 15:21:00 +0100

    Fix cache path

--- src/yahooFinanceService.ts
+++ src/yahooFinanceService.ts
@@ -2,7 +2,7 @@ import * as cacache from "cacache";
 import yahooFinance from 'yahoo-finance2';
 import { YahooFinanceRecord } from './models/yahooFinanceRecord';
 
-const cachePath = process.env.E2G_CACHE_FOLDER || "var/e2g-cache";
+const cachePath = process.env.E2G_CACHE_FOLDER || "/var/e2g-cache";
 
 export class YahooFinanceService {
 
