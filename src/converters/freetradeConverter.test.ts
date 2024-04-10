eee58a17670e9eb5255e6d33e8e83564269ebe21
Colin Seymour <colin@symr.io>
2024-04-10 17:05:39 +0000

    Add another test

--- src/converters/freetradeConverter.test.ts
+++ src/converters/freetradeConverter.test.ts
@@ -101,13 +101,13 @@ describe("freetradeConverter", () => {
     });
   });
 
-  it("should log when Yahoo Finance returns no symbol", (done) => {
+  it("should log when Yahoo Finance returns no ISIN", (done) => {
 
     // Arrange
 
     let tempFileContent = "";
     tempFileContent += "Title,Type,Timestamp,Account Currency,Total Amount,Buy / Sell,Ticker,ISIN,Price per Share in Account Currency,Stamp Duty,Quantity,Venue,Order ID,Order Type,Instrument Currency,Total Shares Amount,Price per Share,FX Rate,Base FX Rate,FX Fee (BPS),FX Fee Amount,Dividend Ex Date,Dividend Pay Date,Dividend Eligible Quantity,Dividend Amount Per Share,Dividend Gross Distribution Amount,Dividend Net Distribution Amount,Dividend Withheld Tax Percentage,Dividend Withheld Tax Amount\n";
-    tempFileContent += `Cisco,ORDER,2024-03-04T11:01:19.356Z,GBP,992.50,BUY,CSCO,US17275R1023,99.25000000,0.00,10.00000000,London Stock Exchange,DJXPPWUNIUCR,BASIC,USD,992.50,99.25000000,,,0,,,,,,,,,`
+    tempFileContent += `Cisco,ORDER,2024-03-04T11:01:19.356Z,GBP,992.50,BUY,,US17275R1023,99.25000000,0.00,10.00000000,London Stock Exchange,DJXPPWUNIUCR,BASIC,USD,992.50,99.25000000,,,0,,,,,,,,,`
 
 
     // Mock Yahoo Finance service to return null.
@@ -122,6 +122,32 @@ describe("freetradeConverter", () => {
     sut.processFileContents(tempFileContent, () => {
 
       expect(consoleSpy).toHaveBeenCalledWith("[i] No result found for buy action for US17275R1023 with currency USD! Please add this manually..\n");
+
+      done();
+    }, () => done.fail("Should not have an error!"));
+  });
+
+  it("should log when Yahoo Finance returns no symbol", (done) => {
+
+    // Arrange
+
+    let tempFileContent = "";
+    tempFileContent += "Title,Type,Timestamp,Account Currency,Total Amount,Buy / Sell,Ticker,ISIN,Price per Share in Account Currency,Stamp Duty,Quantity,Venue,Order ID,Order Type,Instrument Currency,Total Shares Amount,Price per Share,FX Rate,Base FX Rate,FX Fee (BPS),FX Fee Amount,Dividend Ex Date,Dividend Pay Date,Dividend Eligible Quantity,Dividend Amount Per Share,Dividend Gross Distribution Amount,Dividend Net Distribution Amount,Dividend Withheld Tax Percentage,Dividend Withheld Tax Amount\n";
+    tempFileContent += `Cisco,ORDER,2024-03-04T11:01:19.356Z,GBP,992.50,BUY,CSCO,,99.25000000,0.00,10.00000000,London Stock Exchange,DJXPPWUNIUCR,BASIC,USD,992.50,99.25000000,,,0,,,,,,,,,`
+
+
+    // Mock Yahoo Finance service to return null.
+    const yahooFinanceService = new YahooFinanceService();
+    jest.spyOn(yahooFinanceService, "getSecurity").mockImplementation(() => { return null });
+    const sut = new FreetradeConverter(yahooFinanceService);
+
+    // Bit hacky, but it works.
+    const consoleSpy = jest.spyOn((sut as any).progress, "log");
+
+    // Act
+    sut.processFileContents(tempFileContent, () => {
+
+      expect(consoleSpy).toHaveBeenCalledWith("[i] No result found for buy action for CSCO with currency USD! Please add this manually..\n");
 
       done();
     }, () => done.fail("Should not have an error!"));
