42b674dc0869f85f092cf5f38068e1270821e223
Colin Seymour <colin@symr.io>
2024-04-10 16:51:51 +0000

    Use an output folder

--- src/manual.ts
+++ src/manual.ts
@@ -1,4 +1,5 @@
 import { createAndRunConverter } from "./converter";
+import fs from "fs";
 
 // Check if converter was specified.
 if (process.argv.length != 3) {
@@ -10,12 +11,18 @@ else {
 
     // Define import file path.
     const inputFile = process.env.INPUT_FILE;
+    const outputFolder = process.env.E2G_OUTPUT_FOLDER || "tmp/e2g-output";
+
+    // Check if the outputFolder exists, if not create it.
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true });
+    }
 
     // Determine convertor type and run conversion.
     createAndRunConverter(
         process.argv[2].toLocaleLowerCase(),
         inputFile,
-        ".",
+        outputFolder,
         () => { process.exit(0); },
         (error) => {
             console.log(`[e] Error details: ${error}`);
