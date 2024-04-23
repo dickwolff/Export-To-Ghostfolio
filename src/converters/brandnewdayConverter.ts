50dd1197828466f9ad2741cfe0ef6a0a0e04d346
Dick Wolff <dckwlff@gmail.com>
2024-04-23 22:17:46 +0200

    Progress on parsing the BND PDF file

--- src/converters/brandnewdayConverter.ts
+++ src/converters/brandnewdayConverter.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import dayjs from "dayjs";
 import { parse } from "csv-parse";
 import { AbstractConverter } from "./abstractconverter";
@@ -6,8 +7,11 @@ import { GhostfolioExport } from "../models/ghostfolioExport";
 import YahooFinanceRecord from "../models/yahooFinanceRecord";
 import { GhostfolioActivity } from "../models/ghostfolioActivity";
 import { GhostfolioOrderType } from "../models/ghostfolioOrderType";
-import { PdfReader } from "pdfreader";
+// import { PdfReader } from "pdfreader";
 import { AbstractPdfConverter } from "./abstractPdfConverter";
+
+import { getDocument } from "pdfjs-dist";
+import { TextItem } from "pdfjs-dist/types/src/display/api";
 
 export class BrandNewDayConverter extends AbstractPdfConverter {
 
@@ -20,12 +24,71 @@ export class BrandNewDayConverter extends AbstractPdfConverter {
      */
     public readAndProcessFile(filename: string, successCallback: any, errorCallback: any): void {
 
-        new PdfReader({}).parseFileItems(filename, (e, i) => {
-            console.log(e);
-            console.log(i);
-        })
+        if (!fs.existsSync(filename)) {
+            return errorCallback(new Error(`File ${filename} does not exist!`));
+        }
+        // const items = [];
+        // new PdfReader({}).parseFileItems(filename, (err, item) => {
+        //     if (err) errorCallback(err);
+        //     else if (!item) console.log("Leeg");
+        //     else if (item.text) items.push(item);
+        // });
+        // console.log(items)
 
-        successCallback({});
+        getDocument(filename).promise.then(async (pdf) => {
+
+            //for (let idx = 0; idx < pdf.numPages; idx++) {
+            const page = await pdf.getPage(1);
+
+            const textContent = await page.getTextContent();
+            let lines = [];
+            let lastLine = "";
+            let skipColumnBreak = false;
+            for (let itm = 0; itm < textContent.items.length; itm++) {
+                const item = textContent.items[itm] as TextItem;
+
+                // Remove all parsed data before this. Next line will contain transaction data.
+                if (item.str == "Beleggingstransacties") {
+                    lines = [];
+                    lastLine = "";
+                    continue;
+                }
+
+                // Skip spaces
+                if (item.str == " ") {
+                    continue;
+                }
+
+                // Skip line breaks on multi row fields
+                if (item.str == "" && item.transform[4] > 50) {
+                    // console.log(lastLine)
+                    // console.log(item)
+                    skipColumnBreak = true;
+                    continue;
+                }
+
+                // Add to temporary line.
+                if (!item.hasEOL) {
+                    lastLine += `${item.str}${skipColumnBreak ? ' ' : ';'}`;
+
+                    if (skipColumnBreak) {
+                        skipColumnBreak = false;
+                    }
+                }
+
+                // When end of line is found, add completed line to result and clear temporary line.
+                if (item.hasEOL) {
+                    lines.push(lastLine.trim());
+                    lastLine = "";
+                }
+            }
+
+            console.log(lines)
+
+            return successCallback({});
+        });
+
+
     };
 
     /**
