dccc6c3512e2cac888d773f8225d37b78d20c74f
Dick Wolff <5620002+dickwolff@users.noreply.github.com>
2024-10-06 13:26:58 +0200

    Add Parqet converter (#106)

---
+++ src/models/parqetRecord.ts
@@ -0,0 +1,24 @@
+export class ParqetRecord {
+    dateTime: Date;
+    date: Date;
+    time: string;
+    price: number;
+    shares: number;
+    amount: number;
+    tax: number;
+    fee: number;
+    realizedGains: number;
+    type: string;
+    broker: string;
+    assetType: string;
+    identifier: string;
+    wkn: string;
+    originalCurrency: string;
+    currency: string;
+    fxRate: number;
+    holding: string;
+    holdingName: string;
+    holdingNickname: string;
+    exchange: string;
+    avgHoldingPeriod: string
+}
