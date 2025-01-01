ed7e8d6da17da295b6a02761311610f68a9ea4ed
Dick Wolff <dckwlff@gmail.com>
2025-01-01 13:53:47 +0100

    Add dividend support to XTB

--- src/converters/xtbConverter.test.ts
+++ src/converters/xtbConverter.test.ts
@@ -34,7 +34,7 @@ describe("xtbConverter", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(12);
+      expect(actualExport.activities.length).toBe(13);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
