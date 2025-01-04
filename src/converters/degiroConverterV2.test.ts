17d22c1dc4b691ea2db823f9d1744c578b010be5
Dick Wolff <dckwlff@gmail.com>
2025-01-04 14:05:10 +0100

    Fix v2 test

--- src/converters/degiroConverterV2.test.ts
+++ src/converters/degiroConverterV2.test.ts
@@ -35,7 +35,7 @@ describe("degiroConverterV2", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(24);
+      expect(actualExport.activities.length).toBe(25);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
