c7ea9d6aa0df319844efa8ba3c3f01b21e6d7997
Pavel Cvetler <bbcelly@gmail.com>
2025-01-05 17:24:56 +0100

    Added test

--- src/converters/etoroConverter.test.ts
+++ src/converters/etoroConverter.test.ts
@@ -34,7 +34,7 @@ describe("etoroConverter", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(18);
+      expect(actualExport.activities.length).toBe(19);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
