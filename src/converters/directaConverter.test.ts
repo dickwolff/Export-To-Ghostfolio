75ee53dce65776c7da855707274e55b6637abcde
Lorenzo <32000123+lorenzo-deluca@users.noreply.github.com>
2025-01-06 19:49:29 +0100

    Add Directa Converter (#141)

---
+++ src/converters/directaConverter.test.ts
@@ -0,0 +1,130 @@
+import { DirectaConverter } from "./directaConverter";
+import { SecurityService } from "../securityService";
+import { GhostfolioExport } from "../models/ghostfolioExport";
+import YahooFinanceServiceMock from "../testing/yahooFinanceServiceMock";
+
+describe("directaConverter", () => {
+
+  beforeEach(() => {
+    jest.spyOn(console, "log").mockImplementation(jest.fn());
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should construct", () => {
+
+    // Act
+    const sut = new DirectaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+    // Assert
+    expect(sut).toBeTruthy();
+  });
+
+  it("should process sample CSV file", (done) => {
+
+    // Arrange
+    const sut = new DirectaConverter(new SecurityService(new YahooFinanceServiceMock()));
+    const inputFile = "samples/directa-export.csv";
+
+    // Act
+    sut.readAndProcessFile(inputFile, (actualExport: GhostfolioExport) => {
+
+      // Assert
+      expect(actualExport).toBeTruthy();
+      expect(actualExport.activities.length).toBeGreaterThan(0);
+      expect(actualExport.activities.length).toBe(17);
+
+      done();
+    }, () => { done.fail("Should not have an error!"); });
+  });
+
+  describe("should throw an error if", () => {
+    it("the input file does not exist", (done) => {
+
+      // Arrange
+      const sut = new DirectaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileName = "tmp/testinput/directa-filedoesnotexist.csv";
+
+      // Act
+      sut.readAndProcessFile(tempFileName, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+
+        done();
+      });
+    });
+
+    it("the input file is empty", (done) => {
+
+      // Arrange
+      const sut = new DirectaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileContent = "";
+      tempFileContent += "Data operazione,Data valuta,Tipo operazione,Ticker,Isin,Protocollo,Descrizione,Quantità,Importo euro,Importo Divisa,Divisa,Riferimento ordine\n";
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toContain("An error ocurred while parsing");
+
+        done();
+      });
+    });
+
+    it("the header and row column count doesn't match", (done) => {
+
+      // Arrange
+      const sut = new DirectaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileContent = "";
+      tempFileContent += "Data operazione,Data valuta,Tipo operazione,Ticker,Isin,Protocollo,Descrizione,Quantità,Importo euro,Importo Divisa,Divisa,Riferimento ordine\n";
+      tempFileContent += "02-12-2024,04-12-2024,Acquisto,ICOV,IE00B3B8Q275,,ETF COVERED BOND ISH,3,-431.04,0,EUR,XXXXXXXXX,\n";
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toBe("An error ocurred while parsing!");
+
+        done();
+      });
+    });
+  });
+
+  it("should log when Yahoo Finance returns no symbol", (done) => {
+
+    // Arrange
+    let tempFileContent = "";
+    
+    // add fake 10 rows 
+    for (let i = 0; i < 9; i++) {
+      tempFileContent += "\n";
+    }
+    
+    tempFileContent += "Data operazione,Data valuta,Tipo operazione,Ticker,Isin,Protocollo,Descrizione,Quantità,Importo euro,Importo Divisa,Divisa,Riferimento ordine\n";
+    tempFileContent += "02-12-2024,04-12-2024,Acquisto,ICOV,XXXXXXXX,,ETF COVERED BOND ISH,3,-431.04,0,EUR,T3717285639899"
+
+    // Mock Yahoo Finance service to return no quotes.
+    const yahooFinanceServiceMock = new YahooFinanceServiceMock();
+    jest.spyOn(yahooFinanceServiceMock, "search").mockImplementation(() => { return Promise.resolve({ quotes: [] }) });
+    const sut = new DirectaConverter(new SecurityService(yahooFinanceServiceMock));
+
+    // Bit hacky, but it works.
+    const consoleSpy = jest.spyOn((sut as any).progress, "log");
+
+    // Act
+    sut.processFileContents(tempFileContent, () => {
+
+      expect(consoleSpy).toHaveBeenCalledWith("[i] No result found for buy action for XXXXXXXX with currency EUR! Please add this manually..\n");
+
+      done();
+    }, () => done.fail("Should not have an error!"));
+  });
+});
---
+++ src/models/directaRecord.ts
@@ -0,0 +1,14 @@
+export class DirectaRecord {
+    dataOperazione: Date;
+    dataValuta: string;
+    tipoOperazione: string;
+    isin: string;
+    ticker: string;
+    protocollo: string;
+    descrizione: string;
+    quantit: number;
+    importoEuro: number;
+    importoDivisa: number;
+    divisa: string;
+    riferimentoOrdine: string;
+}
