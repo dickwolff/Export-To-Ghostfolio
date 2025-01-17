5bd334def34cea1b491f5b126e18d250bae8b15d
Dick Wolff <5620002+dickwolff@users.noreply.github.com>
2025-01-17 21:29:09 +0000

    Add stock split

--- src/converters/revolutConverter.test.ts
+++ src/converters/revolutConverter.test.ts
@@ -34,7 +34,7 @@ describe("revolutConverter", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(4);
+      expect(actualExport.activities.length).toBe(5);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
