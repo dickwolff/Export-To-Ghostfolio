ab0734cf79eee05dc38dbe49afb1bf8bc0568f57
Dick Wolff <dckwlff@gmail.com>
2025-01-18 14:25:42 +0100

    Dump progress

--- src/converters/revolutConverter.ts
+++ src/converters/revolutConverter.ts
@@ -18,6 +18,24 @@ export class RevolutConverter extends AbstractConverter {
      */
     public processFileContents(input: string, successCallback: any, errorCallback: any): void {
 
+        if (input.split("\n")[0].split(",").length === 6) {
+            return this.processCryptoFileContents(input, successCallback, errorCallback);
+        }
+
+        return this.processInvestFileContents(input, successCallback, errorCallback);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public isIgnoredRecord(record: RevolutRecord): boolean {
+        let ignoredRecordTypes = ["transfer from", "withdrawal", "top-up"];
+
+        return ignoredRecordTypes.some(t => record.type.toLocaleLowerCase().indexOf(t) > -1)
+    }
+
+    private processInvestFileContents(input: string, successCallback: any, errorCallback: any): void {
+
         // Parse the CSV and convert to Ghostfolio import format.
         parse(input, {
             delimiter: ",",
@@ -41,7 +59,7 @@ export class RevolutConverter extends AbstractConverter {
                     else if (action.indexOf("sell") > -1) {
                         return "sell";
                     }
-                    else if (action.indexOf("dividend") > -1 || action.indexOf("reward") > -1) {
+                    else if (action.indexOf("dividend") > -1) {
                         return "dividend";
                     }
                     else if (action.indexOf("fee") > -1) {
@@ -52,136 +70,179 @@ export class RevolutConverter extends AbstractConverter {
                 // Parse numbers to floats (from string).
                 if (context.column === "quantity" ||
                     context.column === "pricePerShare" ||
-                    context.column === "totalAmount" ||
-                    context.column === "price" ||
-                    context.column === "value" ||
-                    context.column === "fees") {
+                    context.column === "totalAmount") {
                     if (columnValue === "") {
                         return 0;
                     }
-                    
+
                     return parseFloat(columnValue.replace(/[$]/g, '').trim());
                 }
 
                 return columnValue;
             }
-        }, async (err, records: RevolutRecord[]) => {
+        }, async (err, records: RevolutRecord[]) => await this.processRevolutFile(err, records, successCallback, errorCallback));
+    }
 
-            // Check if parsing failed..
-            if (err || records === undefined || records.length === 0) {
-                let errorMsg = "An error ocurred while parsing!";
+    private processCryptoFileContents(input: string, successCallback: any, errorCallback: any): void {
 
-                if (err) {
-                    errorMsg += ` Details: ${err.message}`
+        // Parse the CSV and convert to Ghostfolio import format.
+        parse(input, {
+            delimiter: ",",
+            fromLine: 2,
+            columns: this.processHeaders(input),
+            cast: (columnValue, context) => {
+
+                // Custom mapping below.
+
+
+                // Convert actions to Ghostfolio type.
+                if (context.column === "type") {
+                    const action = columnValue.toLocaleLowerCase();
+
+                    if (action.indexOf("buy") > -1) {
+                        return "buy";
+                    }
+                    else if (action.indexOf("sell") > -1) {
+                        return "sell";
+                    }
+                    else if (action.indexOf("reward") > -1) {
+                        return "dividend";
+                    }
                 }
 
-                return errorCallback(new Error(errorMsg))
+                // Parse numbers to floats (from string).
+                if (context.column === "quantity") {
+                    if (columnValue === "") {
+                        return 0;
+                    }
+
+                    return parseFloat(columnValue.replace(/[$]/g, '').trim());
+                }
+
+                // Fields below also contain the currency, so we need to split them.
+                if (context.column === "price" ||
+                    context.column === "value" ||
+                    context.column === "fees") {
+
+                    if (columnValue === "") {
+                        return 0;
+                    }
+
+                    const match = columnValue.match(/^([\d,\.]+)\s*(\w+)$/);
+
+                    return columnValue;
+                }
+            }
+        }, async (err, records: RevolutRecord[]) => await this.processRevolutFile(err, records, successCallback, errorCallback));
+    }
+
+    private async processRevolutFile(err, records: RevolutRecord[], successCallback: any, errorCallback: any) {
+
+        // Check if parsing failed..
+        if (err || records === undefined || records.length === 0) {
+            let errorMsg = "An error ocurred while parsing!";
+
+            if (err) {
+                errorMsg += ` Details: ${err.message}`
             }
 
-            console.log("[i] Read CSV file. Start processing..");
-            const result: GhostfolioExport = {
-                meta: {
-                    date: new Date(),
-                    version: "v0"
-                },
-                activities: []
+            return errorCallback(new Error(errorMsg))
+        }
+
+        console.log("[i] Read CSV file. Start processing..");
+        const result: GhostfolioExport = {
+            meta: {
+                date: new Date(),
+                version: "v0"
+            },
+            activities: []
+        }
+
+        // Populate the progress bar.
+        const bar1 = this.progress.create(records.length, 0);
+
+        for (let idx = 0; idx < records.length; idx++) {
+            const record = records[idx];
+            console.log(record)
+            // Check if the record should be ignored.
+            if (this.isIgnoredRecord(record)) {
+                bar1.increment();
+                continue;
             }
 
-            // Populate the progress bar.
-            const bar1 = this.progress.create(records.length, 0);
+            // Fees do not have a security, so add those immediately.
+            if (record.type.toLocaleLowerCase() === "fee") {
 
-            for (let idx = 0; idx < records.length; idx++) {
-                const record = records[idx];
-console.log(record)
-                // Check if the record should be ignored.
-                if (this.isIgnoredRecord(record)) {
-                    bar1.increment();
-                    continue;
-                }
+                const feeAmount = Math.abs(record.totalAmount);
 
-                // Fees do not have a security, so add those immediately.
-                if (record.type.toLocaleLowerCase() === "fee") {
-
-                    const feeAmount = Math.abs(record.totalAmount);
-
-                    // Add record to export.
-                    result.activities.push({
-                        accountId: process.env.GHOSTFOLIO_ACCOUNT_ID,
-                        comment: `Revolut ${record.type.toLocaleLowerCase()}`,
-                        fee: feeAmount,
-                        quantity: 1,
-                        type: GhostfolioOrderType[record.type],
-                        unitPrice: 0,
-                        currency: record.currency,
-                        dataSource: "MANUAL",
-                        date: dayjs(record.date).format("YYYY-MM-DDTHH:mm:ssZ"),
-                        symbol: `Revolut ${record.type.toLocaleLowerCase()}`
-                    });
-
-                    bar1.increment();
-                    continue;
-                }
-
-                let security: YahooFinanceRecord;
-                try {
-                    security = await this.securityService.getSecurity(
-                        null,
-                        record.ticker ?? record.symbol,
-                        null,
-                        record.currency,
-                        this.progress);
-                }
-                catch (err) {
-                    this.logQueryError(record.ticker, idx + 2);
-                    return errorCallback(err);
-                }
-
-                // Log whenever there was no match found.
-                if (!security) {
-                    this.progress.log(`[i] No result found for ${record.type} action for ${record.ticker} with currency ${record.currency}! Please add this manually..\n`);
-                    bar1.increment();
-                    continue;
-                }
-
-                let quantity, unitPrice;
-
-                if (record.type === "dividend") {
-                    quantity = 1;
-                    unitPrice = Math.abs(record.totalAmount);
-                } else {
-                    quantity = record.quantity;
-                    unitPrice = record.pricePerShare;
-                }
-console.log(record)
                 // Add record to export.
                 result.activities.push({
                     accountId: process.env.GHOSTFOLIO_ACCOUNT_ID,
-                    comment: "",
-                    fee: 0,
-                    quantity: quantity,
+                    comment: `Revolut ${record.type.toLocaleLowerCase()}`,
+                    fee: feeAmount,
+                    quantity: 1,
                     type: GhostfolioOrderType[record.type],
-                    unitPrice: unitPrice,
+                    unitPrice: 0,
                     currency: record.currency,
-                    dataSource: "YAHOO",
+                    dataSource: "MANUAL",
                     date: dayjs(record.date).format("YYYY-MM-DDTHH:mm:ssZ"),
-                    symbol: security.symbol
+                    symbol: `Revolut ${record.type.toLocaleLowerCase()}`
                 });
 
                 bar1.increment();
+                continue;
             }
 
-            this.progress.stop()
+            let security: YahooFinanceRecord;
+            try {
+                security = await this.securityService.getSecurity(
+                    null,
+                    record.ticker ?? record.symbol,
+                    null,
+                    record.currency,
+                    this.progress);
+            }
+            catch (err) {
+                this.logQueryError(record.ticker, idx + 2);
+                return errorCallback(err);
+            }
 
-            successCallback(result);
-        });
-    }
+            // Log whenever there was no match found.
+            if (!security) {
+                this.progress.log(`[i] No result found for ${record.type} action for ${record.ticker} with currency ${record.currency}! Please add this manually..\n`);
+                bar1.increment();
+                continue;
+            }
 
-    /**
-     * @inheritdoc
-     */
-    public isIgnoredRecord(record: RevolutRecord): boolean {
-        let ignoredRecordTypes = ["transfer from", "withdrawal", "top-up"];
+            let quantity, unitPrice;
 
-        return ignoredRecordTypes.some(t => record.type.toLocaleLowerCase().indexOf(t) > -1)
+            if (record.type === "dividend") {
+                quantity = 1;
+                unitPrice = Math.abs(record.totalAmount);
+            } else {
+                quantity = record.quantity;
+                unitPrice = record.pricePerShare;
+            }
+            console.log(record)
+            // Add record to export.
+            result.activities.push({
+                accountId: process.env.GHOSTFOLIO_ACCOUNT_ID,
+                comment: "",
+                fee: 0,
+                quantity: quantity,
+                type: GhostfolioOrderType[record.type],
+                unitPrice: unitPrice,
+                currency: record.currency,
+                dataSource: "YAHOO",
+                date: dayjs(record.date).format("YYYY-MM-DDTHH:mm:ssZ"),
+                symbol: security.symbol
+            });
+
+            bar1.increment();
+        }
+
+        this.progress.stop()
+
+        successCallback(result);
     }
 }
