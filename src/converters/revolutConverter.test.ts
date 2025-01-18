9caf0ff042e4ca42d68b3b83f9410896dbf10bdc
Dick Wolff <dckwlff@gmail.com>
2025-01-18 21:13:26 +0100

    Add testdata

--- src/converters/revolutConverter.test.ts
+++ src/converters/revolutConverter.test.ts
@@ -53,7 +53,7 @@ describe("revolutConverter", () => {
         // Assert
         expect(actualExport).toBeTruthy();
         expect(actualExport.activities.length).toBeGreaterThan(0);
-        expect(actualExport.activities.length).toBe(9);
+        expect(actualExport.activities.length).toBe(5); // Currently only 5 because of BTC-SEK not existing.
 
         done();
       }, () => { done.fail("Should not have an error!"); });
