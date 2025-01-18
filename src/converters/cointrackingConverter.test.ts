2a1ed87bd62033ca090e2dc659bf64a13426c558
Dick Wolff <dckwlff@gmail.com>
2025-01-18 15:08:39 +0100

    Add tests for CoinTracking

---
+++ src/converters/cointrackingConverter.test.ts
@@ -0,0 +1,147 @@
+import { SecurityService } from "../securityService";
+import { GhostfolioExport } from "../models/ghostfolioExport";
+import { CointrackingConverter } from "./cointrackingConverter";
+import YahooFinanceServiceMock from "../testing/yahooFinanceServiceMock";
+
+describe("cointrackingConverter", () => {
+
+  beforeEach(() => {
+    jest.spyOn(console, "log").mockImplementation(jest.fn());
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should construct", () => {
+
+    // Act
+    const sut = new CointrackingConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+    // Assert
+    expect(sut).toBeTruthy();
+  });
+
+  it("should process sample CSV file", (done) => {
+
+    // Arange
+    const sut = new CointrackingConverter(new SecurityService(new YahooFinanceServiceMock()));
+    const inputFile = "samples/cointracking-export.csv";
+
+    // Act
+    sut.readAndProcessFile(inputFile, (actualExport: GhostfolioExport) => {
+
+      // Assert
+      expect(actualExport).toBeTruthy();
+      expect(actualExport.activities.length).toBeGreaterThan(0);
+      expect(actualExport.activities.length).toBe(3);
+
+      done();
+    }, () => { done.fail("Should not have an error!"); });
+  });
+
+  describe("should throw an error if", () => {
+    it("the input file does not exist", (done) => {
+
+      // Arrange
+      const sut = new CointrackingConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileName = "tmp/testinput/cointracking-filedoesnotexist.csv";
+
+      // Act
+      sut.readAndProcessFile(tempFileName, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+
+        done();
+      });
+    });
+
+    it("the input file is empty", (done) => {
+
+      // Arrange
+      const sut = new CointrackingConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileContent = "";
+      tempFileContent += `"Type","Buy","Cur.","Sell","Cur.","Fee","Cur.","Exchange","Group","Comment","Date","Tx-ID"\n`;
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toContain("An error ocurred while parsing");
+
+        done();
+      });
+    });
+
+    it("the header and row column count doesn't match", (done) => {
+
+      // Arrange
+      const sut = new CointrackingConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileContent = "";
+      tempFileContent += `"Type","Buy","Cur.","Sell","Cur.","Fee","Cur.","Exchange","Group","Comment","Date","Tx-ID"\n`;
+      tempFileContent += `"Trade","0.00550000","BTC","500.00000000","EUR","19.00000000","EUR","Kraken","","","2025-01-01 10:12:27","",,`;
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toBe("An error ocurred while parsing! Details: Invalid Record Length: columns length is 16, got 18 on line 2");
+
+        done();
+      });
+    });
+
+    it("Yahoo Finance throws an error", (done) => {
+
+      // Arrange
+      let tempFileContent = "";
+      tempFileContent += `"Type","Buy","Cur.","Sell","Cur.","Fee","Cur.","Exchange","Group","Comment","Date","Tx-ID"\n`;
+      tempFileContent += `"Trade","0.00550000","BTC","500.00000000","EUR","19.00000000","EUR","Kraken","","","2025-01-01 10:12:27",""`;
+
+      // Mock Yahoo Finance service to throw error.
+      const yahooFinanceServiceMock = new YahooFinanceServiceMock();
+      jest.spyOn(yahooFinanceServiceMock, "search").mockImplementation(() => { throw new Error("Unit test error"); });
+      const sut = new CointrackingConverter(new SecurityService(yahooFinanceServiceMock));
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toContain("Unit test error");
+
+        done();
+      });
+    });
+  });
+
+  it("should log when Yahoo Finance returns no symbol", (done) => {
+
+    // Arrange
+    let tempFileContent = "";
+    tempFileContent += `"Type","Buy","Cur.","Sell","Cur.","Fee","Cur.","Exchange","Group","Comment","Date","Tx-ID"\n`;
+    tempFileContent += `"Trade","0.00550000","BTC","500.00000000","EUR","19.00000000","EUR","Kraken","","","2025-01-01 10:12:27",""`;
+
+    // Mock Yahoo Finance service to return no quotes.
+    const yahooFinanceServiceMock = new YahooFinanceServiceMock();
+    jest.spyOn(yahooFinanceServiceMock, "search").mockImplementation(() => { return Promise.resolve({ quotes: [] }) });
+    const sut = new CointrackingConverter(new SecurityService(yahooFinanceServiceMock));
+
+    // Bit hacky, but it works.
+    const consoleSpy = jest.spyOn((sut as any).progress, "log");
+
+    // Act
+    sut.processFileContents(tempFileContent, () => {
+
+      expect(consoleSpy).toHaveBeenCalledWith("[i] No result found trade with currency BTC-EUR! Please add this manually..\n");
+
+      done();
+    }, () => done.fail("Should not have an error!"));
+  });
+});
