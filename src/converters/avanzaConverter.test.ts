de4c6fb569593c937a3d5616583f0a411167d61f
Dick Wolff <dckwlff@gmail.com>
2025-01-20 20:24:14 +0100

    Add tests

---
+++ src/converters/avanzaConverter.test.ts
@@ -0,0 +1,147 @@
+import { AvanzaConverter } from "./avanzaConverter";
+import { SecurityService } from "../securityService";
+import { GhostfolioExport } from "../models/ghostfolioExport";
+import YahooFinanceServiceMock from "../testing/yahooFinanceServiceMock";
+
+describe("avanzaConverter", () => {
+
+  beforeEach(() => {
+    jest.spyOn(console, "log").mockImplementation(jest.fn());
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should construct", () => {
+
+    // Act
+    const sut = new AvanzaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+    // Assert
+    expect(sut).toBeTruthy();
+  });
+
+  it("should process sample CSV file", (done) => {
+
+    // Arange
+    const sut = new AvanzaConverter(new SecurityService(new YahooFinanceServiceMock()));
+    const inputFile = "samples/avanza-export.csv";
+
+    // Act
+    sut.readAndProcessFile(inputFile, (actualExport: GhostfolioExport) => {
+
+      // Assert
+      expect(actualExport).toBeTruthy();
+      expect(actualExport.activities.length).toBeGreaterThan(0);
+      expect(actualExport.activities.length).toBe(6);
+
+      done();
+    }, () => { done.fail("Should not have an error!"); });
+  });
+
+  describe("should throw an error if", () => {
+    it("the input file does not exist", (done) => {
+
+      // Arrange
+      const sut = new AvanzaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileName = "tmp/testinput/avanza-filedoesnotexist.csv";
+
+      // Act
+      sut.readAndProcessFile(tempFileName, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+
+        done();
+      });
+    });
+
+    it("the input file is empty", (done) => {
+
+      // Arrange
+      const sut = new AvanzaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileContent = "";
+      tempFileContent += "Datum;Konto;Typ av transaktion;Värdepapper/beskrivning;Antal;Kurs;Belopp;Transaktionsvaluta;Courtage (SEK);Valutakurs;Instrumentvaluta;ISIN;Resultat\n";
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toContain("An error ocurred while parsing");
+
+        done();
+      });
+    });
+
+    it("the header and row column count doesn't match", (done) => {
+
+      // Arrange
+      const sut = new AvanzaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+      let tempFileContent = "";
+      tempFileContent += "Datum;Konto;Typ av transaktion;Värdepapper/beskrivning;Antal;Kurs;Belopp;Transaktionsvaluta;Courtage (SEK);Valutakurs;Instrumentvaluta;ISIN;Resultat\n";
+      tempFileContent += `2025-01-13;Savings;Sälj;DNB Global Indeks S;-352,033838;154,665276;54447,41;SEK;0;;SEK;NO0010827280;14650,376974;;`;
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toBe("An error ocurred while parsing! Details: Invalid Record Length: columns length is 13, got 15 on line 2");
+
+        done();
+      });
+    });
+
+    it("Yahoo Finance throws an error", (done) => {
+
+      // Arrange
+      let tempFileContent = "";
+      tempFileContent += "Datum;Konto;Typ av transaktion;Värdepapper/beskrivning;Antal;Kurs;Belopp;Transaktionsvaluta;Courtage (SEK);Valutakurs;Instrumentvaluta;ISIN;Resultat\n";
+      tempFileContent += `2025-01-13;Savings;Sälj;DNB Global Indeks S;-352,033838;154,665276;54447,41;SEK;0;;SEK;NO0010827280;14650,376974`;
+
+      // Mock Yahoo Finance service to throw error.
+      const yahooFinanceServiceMock = new YahooFinanceServiceMock();
+      jest.spyOn(yahooFinanceServiceMock, "search").mockImplementation(() => { throw new Error("Unit test error"); });
+      const sut = new AvanzaConverter(new SecurityService(yahooFinanceServiceMock));
+
+      // Act
+      sut.processFileContents(tempFileContent, () => { done.fail("Should not succeed!"); }, (err: Error) => {
+
+        // Assert
+        expect(err).toBeTruthy();
+        expect(err.message).toContain("Unit test error");
+
+        done();
+      });
+    });
+  });
+
+  it("should log when Yahoo Finance returns no symbol", (done) => {
+
+    // Arrange
+    let tempFileContent = "";
+    tempFileContent += "Datum;Konto;Typ av transaktion;Värdepapper/beskrivning;Antal;Kurs;Belopp;Transaktionsvaluta;Courtage (SEK);Valutakurs;Instrumentvaluta;ISIN;Resultat\n";
+    tempFileContent += `2025-01-13;Savings;Sälj;DNB Global Indeks S;-352,033838;154,665276;54447,41;SEK;0;;SEK;NO0010827280;14650,376974`;
+
+    // Mock Yahoo Finance service to return no quotes.
+    const yahooFinanceServiceMock = new YahooFinanceServiceMock();
+    jest.spyOn(yahooFinanceServiceMock, "search").mockImplementation(() => { return Promise.resolve({ quotes: [] }) });
+    const sut = new AvanzaConverter(new SecurityService(yahooFinanceServiceMock));
+
+    // Bit hacky, but it works.
+    const consoleSpy = jest.spyOn((sut as any).progress, "log");
+
+    // Act
+    sut.processFileContents(tempFileContent, () => {
+
+      expect(consoleSpy).toHaveBeenCalledWith("[i] No result found for sell action for NO0010827280 with currency SEK! Please add this manually..\n");
+
+      done();
+    }, () => done.fail("Should not have an error!"));
+  });
+});
