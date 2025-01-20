8420190cf45f19d44eeafa2d07eb5f9a2752519b
Dick Wolff <dckwlff@gmail.com>
2025-01-20 13:59:48 +0100

    Update some testcases to improve coverage

--- src/converters/xtbConverter.test.ts
+++ src/converters/xtbConverter.test.ts
@@ -34,7 +34,7 @@ describe("xtbConverter", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(32);
+      expect(actualExport.activities.length).toBe(33);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
