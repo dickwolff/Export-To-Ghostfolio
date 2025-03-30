05b9c5195fafb314aab376c232ca82b12839a90c
Dick Wolff <dckwlff@gmail.com>
2025-03-30 14:53:23 +0200

    Update tests

--- src/converters/schwabConverter.test.ts
+++ src/converters/schwabConverter.test.ts
@@ -34,7 +34,7 @@ describe("schwabConverter", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(103);
+      expect(actualExport.activities.length).toBe(105);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
