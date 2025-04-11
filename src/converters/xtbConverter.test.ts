37e0e0b7da59dd2114e9163b0834e6b24aa641eb
Dick Wolff <dckwlff@gmail.com>
2025-04-11 14:02:25 +0200

    Also update sale

--- src/converters/xtbConverter.test.ts
+++ src/converters/xtbConverter.test.ts
@@ -34,7 +34,7 @@ describe("xtbConverter", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(38);
+      expect(actualExport.activities.length).toBe(40);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
