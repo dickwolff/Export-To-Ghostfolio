3516a69b5f84e6fd5696d66d4d7a8f4a2e614bd3
Dick Wolff <dckwlff@gmail.com>
2025-05-02 14:35:47 +0200

    Also look for English header for TradeRepublic

--- src/watcher.ts
+++ src/watcher.ts
@@ -122,5 +122,6 @@ headers.set(`Client ID,Trade Date,Value Date,Type,Instrument,Instrument ISIN,Ins
 headers.set(`Date,Action,Symbol,Description,Quantity,Price,Fees & Comm,Amount`, "schwab");
 headers.set(`Date;Order #;Transaction;Symbol;Name;ISIN;Quantity;Unit price;Costs;Accrued Interest;Net Amount;Balance;Currency`, "swissquote");
 headers.set(`Datum;Transactietype;Waarde (netto);Opmerking;ISIN;Aantal;Kosten;Belasting`, "tradeRepublic");
+headers.set(`Date;Type;Value;Note;ISIN;Shares;Fees;Taxes`, "tradeRepublic");
 headers.set(`Action,Time,ISIN,Ticker,Name,No. of shares,Price / share,Currency (Price / share),Exchange rate,Result,Currency (Result),Total,Currency (Total),Withholding tax,Currency (Withholding tax),Notes,ID,Currency conversion fee`, "trading212");
 headers.set(`ID;Type;Time;Symbol;Comment;Amount`, "xtb");
