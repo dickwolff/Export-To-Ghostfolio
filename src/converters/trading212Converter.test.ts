b7a3bc47b6bb7282dc54131c86eab04aa8dd6818
Dick Wolff <dckwlff@gmail.com>
2025-05-26 08:21:27 +0200

    T212: Add stock split

--- src/converters/trading212Converter.test.ts
+++ src/converters/trading212Converter.test.ts
@@ -34,7 +34,7 @@ describe("trading212Converter", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(9);
+      expect(actualExport.activities.length).toBe(11);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
