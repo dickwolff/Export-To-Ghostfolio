fa36db2374cfd82be34766fd575ec36dac7d912f
Dick Wolff <dckwlff@gmail.com>
2025-07-06 13:54:12 +0200

    Add tests

--- src/converters/avanzaConverter.test.ts
+++ src/converters/avanzaConverter.test.ts
@@ -144,4 +144,26 @@ describe("avanzaConverter", () => {
       done();
     }, () => done.fail("Should not have an error!"));
   });
+
+  it("should log error and invoke errorCallback when an error occurs in processFileContents", (done) => {
+
+    // Arrange
+    const tempFileContent = "ID;Type;Time;Symbol;Comment;Amount\n";
+    const sut = new AvanzaConverter(new SecurityService(new YahooFinanceServiceMock()));
+
+    const consoleSpy = jest.spyOn(console, "log");
+
+    // Act
+    sut.processFileContents(tempFileContent, () => {
+      done.fail("Should not succeed!");
+    }, (err: Error) => {
+     
+      // Assert
+      expect(consoleSpy).toHaveBeenCalledWith("[e] An error occurred while processing the file contents. Stack trace:");
+      expect(consoleSpy).toHaveBeenCalledWith(err.stack);
+      expect(err).toBeTruthy();
+
+      done();
+    });
+  });
 });
