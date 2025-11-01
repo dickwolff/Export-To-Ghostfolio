7c4dccbafc94efcf0b4d67d55c770d33fc422021
swissbuechi <24353767+swissbuechi@users.noreply.github.com>
2025-11-01 22:58:38 +0100

    Fixed finpension interest tests

--- src/converters/finpensionConverter.test.ts
+++ src/converters/finpensionConverter.test.ts
@@ -34,7 +34,7 @@ describe("finpensionConverter", () => {
       // Assert
       expect(actualExport).toBeTruthy();
       expect(actualExport.activities.length).toBeGreaterThan(0);
-      expect(actualExport.activities.length).toBe(26);
+      expect(actualExport.activities.length).toBe(25);
 
       done();
     }, () => { done.fail("Should not have an error!"); });
