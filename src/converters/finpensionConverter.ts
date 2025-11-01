99aba479394d8e73cf3bfe79086d8672365d5b2f
swissbuechi <24353767+swissbuechi@users.noreply.github.com>
2025-11-01 23:02:18 +0100

    Added comment default value fix from upstream

--- src/converters/finpensionConverter.ts
+++ src/converters/finpensionConverter.ts
@@ -176,7 +176,7 @@ export class FinpensionConverter extends AbstractConverter {
                     // Add record to export.
                     result.activities.push({
                         accountId: process.env.GHOSTFOLIO_ACCOUNT_ID,
-                        comment: "",
+                        comment: null,
                         fee: 0,
                         quantity: numberOfShares,
                         type: GhostfolioOrderType[record.category],
