bef79c6d00e215f8396c6248ea0ff60654ab3f20
Dick Wolff <dckwlff@gmail.com>
2025-11-08 10:21:18 +0100

    Fix test

--- src/converters/disnatConverter.test.ts
+++ src/converters/disnatConverter.test.ts
@@ -34,7 +34,7 @@ describe("disnatConverter", () => {
             // Assert
             expect(actualExport).toBeTruthy();
             expect(actualExport.activities.length).toBeGreaterThan(0);
-            expect(actualExport.activities.length).toBe(17);
+            expect(actualExport.activities.length).toBe(15);
 
             done();
         }, () => { done.fail("Should not have an error!"); });
